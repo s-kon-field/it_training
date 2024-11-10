df5c8c81d9a20fe8e16380aeb16e92f6a4037e86
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-10 16:10:44 +0900

    ここ

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburgerMenu = document.getElementById('hamburger-menu');
-    var hamburgerMenuContent = document.getElementById('hamburger-menu-content');
-
-    hamburgerMenu.addEventListener('click', function() {
-        // ナビゲーションメニューの表示/非表示を切り替え
-        if (hamburgerMenuContent.style.display === 'block') {
-            hamburgerMenuContent.style.display = 'none';
-        } else {
-            hamburgerMenuContent.style.display = 'block';
-        }
-    });
-});
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+  }
