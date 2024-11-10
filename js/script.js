af44c1f05f570ce75bfed6edcd341178bae047f2
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-11-10 16:29:09 +0900

    opoop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-  }
+$(function(){
+
+	var agent = navigator.userAgent;
+	var list = $(".mod_nav ul"); 
+	
+	function menuslide(){
+	//メニュー表示非表示
+	$(".mod_nav p.navtitle").click(function(){
+	if($(list).css("display")=="none"){
+	$(list).slideDown("fast");
+	}else{
+	$(list).slideUp("fast");
+	}
+	});
+	}
+
+	if(agent.search(/iPhone/) != -1){
+		menuslide();
+	
+	}else if(agent.search(/Android/) != -1){
+		menuslide();
+	
+	}else{
+//resize
+	$(window).resize(function(){
+	var w = $(window).width();
+	var x = 600;
+		if (w <= x) {
+		$(list).css("display","none");
+		} else {
+		$(list).css("display","block");
+		}
+	});
+		menuslide();
+	}
+});
